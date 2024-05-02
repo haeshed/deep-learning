dbe41a57d0d000d87cec0546da41550dd022dbae
haeshed <ha.eshed@gmail.com>
2024-05-02 18:13:08 +0300

    p2 q1, q2

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -47,26 +47,41 @@ test set.
 part2_q1 = r"""
 **Your answer:**
 
+In the SVM loss function 𝐿(𝑊) as defined above, the parameter Δ represents the margin,
+ which is a hyperparameter that controls the trade-off between the margin and the loss incurred by violating the margin. 
+ The choice of Δ is arbitrary
+   in the sense that it determines the threshold beyond which the model adds a loss for misclassification.<br>
+    A larger Δ will result in a larger margin, which may lead to a simpler model that generalizes better to unseen data. <br>
+    However, a larger margin may also result in a higher loss for misclassified samples, which may lead to a model that
+    underfits the training data. <br>
+    On the other hand, a smaller Δ will result in a smaller margin, which may lead to a
+    more complex model that overfits the training data. <br> Therefore, the choice of Δ should be made based on the specific
+    dataset and the desired trade-off between model complexity and generalization performance.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
 part2_q2 = r"""
 **Your answer:**
 
+In a linear model for classification, such as the SVM model, 
+the model learns a linear decision boundary using a set of weights over the given data. <br> 
+I our case of handwritten digits, the computer receives pixel intensity values matrices (28x28 = 784), 
+and needs to learn some relevant features from it. <br>
+The model may learn patterns associated with different digits based on their pixel intensity distributions and patterns. <br><br>
+The MNIST database should allow our model to learn that a digit 1 correspondes to a straight line, while the digit 8
+corresponds to 2 circles in a vertical orientation. <br>This is very shallow learning, as it does not account for translation, line thickness etc.
+(these could be better learned using a CNN for example). <br>This also means that if the database does not contain uniformally distributed (or close)
+for  changes in the handwriting style, then the model could learn (for example)<br> that thick lines correspondes to a 7, 
+which is obviously not something we want to acheive. <br><br>
+According to the visualization section of our notebook, we can observe what are the feature weights for each digit.<br>
+In our human eyes, these does not mean much, but we can try to infer, for example that  0 is closest to 9 (maybe because of the circle they share?) <br>
+and that 3 is quite close to 5 (maybe because they both have a half circular leg and some top?). <br><br>
+We can observe the errors of classification and see that in some cases, they are quite understandable. <br>
+For example. if the handwritten 9 digit is not closed at the top of the circle, it surly resembles a 4. <br>
+Or, if a 6 is written with a down extension of the circle, it resembles a 4. <br>
+Other errors are more difficult to explain, this is due to the complex nature of the weights and the method.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
